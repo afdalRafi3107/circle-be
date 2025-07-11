bd91cf7e1bac3c276c9cd76d0a638962494abaaa
afdalrafi3107 <afdalrafi990@gmail.com>
2025-07-11 15:34:25 +0700

    feat: edit app ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+import cors from "cors";
 import express from "express";
 import router from "./routes/route";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 
-export const app = express();
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT;
 app.use(
   cors({
     origin: [
@@ -12,11 +13,11 @@ app.use(
     ],
   })
 );
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(cookieParser());
-app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
+app.use(express.urlencoded());
 app.use("/api/v1", router);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
