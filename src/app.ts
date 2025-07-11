77dcadc310c34521b5830e38ba25d1b0e6cd461f
afdalrafi3107 <afdalrafi990@gmail.com>
2025-07-11 14:16:11 +0700

    fix: configuration for deploy

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
 
-const app = express();
+export const app = express();
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use(cors({
   origin: ["http://localhost:5173", "https://circle-git-main-muhammad-afdal-rafis-projects.vercel.app"]
@@ -15,6 +15,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 app.use("/api/v1", router);
-app.listen(process.env.PORT, () => {
-  console.log("Server Is Running");
-});
+
