013b056f2756518c2ee6f6d41cfb72cc5e188433
afdalrafi3107 <afdalrafi990@gmail.com>
2025-07-11 11:38:01 +0700

    feat: add cors url

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@ import path from "path";
 
 const app = express();
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://circle-git-main-muhammad-afdal-rafis-projects.vercel.app"]
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.get('/', (req, res) => {
